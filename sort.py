81d0f20598106b8a95eb8e178d805080fff5bcca
Diego <diegosantos4957@gmail.com>
2025-12-08 21:06:11 -0300

    add __init__

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-notes = [0,2,3,5,2,1,4,7,9,6,8]
+notes = [10,2,3,5,2,1,4,7,9,6,8,0]
 
 # Sort List in O(n) time complexity
 def counting_sort(arr):
@@ -8,8 +8,12 @@ def counting_sort(arr):
     max_val = max(arr)
     count = [0] * (max_val + 1)
 
+    print(f"Initial count array: {count}")
+
     for num in arr:
         count[num] += 1
+
+    print(f"Count array after counting elements: {count}")
 
     sorted_index = 0
     for i in range(len(count)):
@@ -17,8 +21,8 @@ def counting_sort(arr):
             arr[sorted_index] = i
             sorted_index += 1
             count[i] -= 1
-
     return arr
 
-sorted_notes = counting_sort(notes)
-print("Sorted notes:", sorted_notes)
+if __name__ == "__main__":
+    sorted_notes = counting_sort(notes)
+    print("Sorted notes:", sorted_notes)
