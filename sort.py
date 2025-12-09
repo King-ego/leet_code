feb06c2c4f9ead7e022991981f49857aa652c297
Diego <diegosantos4957@gmail.com>
2025-12-08 21:00:15 -0300

    sort with two for

---
+++ sort.py
@@ -0,0 +1,24 @@
+notes = [0,2,3,5,2,1,4,7,9,6,8]
+
+# Sort List in O(n) time complexity
+def counting_sort(arr):
+    if not arr:
+        return arr
+
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    for num in arr:
+        count[num] += 1
+
+    sorted_index = 0
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr[sorted_index] = i
+            sorted_index += 1
+            count[i] -= 1
+
+    return arr
+
+sorted_notes = counting_sort(notes)
+print("Sorted notes:", sorted_notes)
